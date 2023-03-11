e2183200f1ed5126e2453662815c26548d893f34
Welby Seely <welbyseely@gmail.com>
2023-03-11 15:42:49 -0700

    Added client

--- src/main/java/com/mangomelancholy/mangoai/adapters/outbound/DavinciService.java
+++ src/main/java/com/mangomelancholy/mangoai/adapters/outbound/DavinciService.java
@@ -3,8 +3,13 @@ package com.mangomelancholy.mangoai.adapters.outbound;
 import com.mangomelancholy.mangoai.application.ports.secondary.AIService;
 import com.mangomelancholy.mangoai.application.conversation.ConversationEntity;
 import com.mangomelancholy.mangoai.application.conversation.ExpressionValue;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-public class AIServiceImpl implements AIService {
+@Service
+public class DavinciService implements AIService {
+
+
 
   @Override
   public ExpressionValue exchange(final ConversationEntity conversationEntity) {
---
+++ src/main/java/com/mangomelancholy/mangoai/infrastructure/OpenAICompletionsClient.java
@@ -0,0 +1,41 @@
+package com.mangomelancholy.mangoai.infrastructure;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Component
+public class OpenAICompletionsClient {
+
+    public OpenAICompletionsClient(@Value("${pal.secrets.authkey}") final String apiKey) {
+        this.webClient = WebClient.create("https://api.openai.com/v1/");
+        this.apiKey = apiKey;
+    }
+    private final WebClient webClient;
+    private final String apiKey;
+
+
+    public Mono<String> complete(final String prompt) {
+        final OpenAIRequest request = new OpenAIRequest.Builder()
+            .model("text-davinci-003")
+            .prompt(prompt)
+            .temperature(0.5)
+            .maxTokens(4000)
+            .topP(0.3)
+            .frequencyPenalty(0.5)
+            .presencePenalty(0)
+            .stream(false)
+            .build();
+
+        return webClient.post()
+            .uri("completions")
+            .header("Content-Type", "application/json")
+            .header("Authorization", "Bearer " + apiKey)
+            .body(BodyInserters.fromValue(request))
+            .retrieve()
+            .bodyToMono(String.class);
+    }
+
+}
